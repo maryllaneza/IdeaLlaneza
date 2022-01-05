3c482ea9895b5085ff26019e6dab63b2f8458b3f
maryllaneza <77010772+maryllaneza@users.noreply.github.com>
2022-01-04 21:51:47 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NavBar (){
+    return(
+        <div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Inicio</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Interior</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Exterior</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Suculentas y Cactus</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">DecoNatural</a></li>
+      </ul>
+    </header>
+  </div>
+    )
+}
